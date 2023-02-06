a51dee4d64b2a71046a227bbd5ee90845589d796
Diwakar <developer.diwakar@gmail.com>
2023-02-07 00:40:44 +0530

    Message and Topic models added

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Room
+from .models import Room, Message, Topic
 
 # register the Room section into the Admin Dashboards:
 admin.site.register(Room)
+admin.site.register(Message)
+admin.site.register(Topic)
